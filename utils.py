5b10f9d76eb9bb669e396299bf25431b98062701
RJZheng1 <stuykid2016@yahoo.com>
2015-11-29 17:03:49 -0500

    Did some bug fixing.

--- utils.py
+++ utils.py
@@ -18,15 +18,15 @@ def getQuery(query):
         elif(q == 2):
             list(map(lambda n: pnames.extend(list(findDates(n))), soup.find_all(string = re.compile('((?:(?:January|February|March|April|May|June|July|August|September|October|November|December) (?:[1-3]?[1-9], )?)?[1-9]{4})'))))
         elif(q == 3):
-            pnames = soup.find_all(string = re.compile('[0-9]+ [A-Z][a-z]+|[A-Z][a-z]+|[0-9]+ [A-Z][a-z]+ [A-Z][a-z]+|[A-Z][a-z]+ [A-Z][a-z]+|[A-Z][a-z]+, [A-Z][a-z]+|[A-Z][a-z]+ [A-Z][a-z]+, [A-Z][a-z]+'))
+            list(map(lambda n: pnames.extend(list(findPlaces(n))), soup.find_all(string = re.compile('(?:[0-9]+ [A-Z][a-z]+)|(?:[A-Z][a-z]+)|(?:[0-9]+ [A-Z][a-z]+ [A-Z][a-z]+)|(?:[A-Z][a-z]+ [A-Z][a-z]+)|(?:[A-Z][a-z]+, [A-Z][a-z]+)|(?:[A-Z][a-z]+ [A-Z][a-z]+, [A-Z][a-z]+)'))))
         for n in pnames:
             if(n in names):
                 names[n] += 1
             else:
                 names[n] = 1
-                sortedNames = sorted(names.items(), key = lambda x: x[1], reverse = True)
-                print names[sortedNames[0][0]]
-        return sortedNames[0][0]
+    sortedNames = sorted(names.items(), key = lambda x: x[1], reverse = True)
+    print names[sortedNames[0][0]]
+    return sortedNames[0][0]
             
 def findNames(name):
     pattern = re.compile('[A-Z][a-z]+ [A-Z][a-z]+')
@@ -36,6 +36,10 @@ def findDates(date):
     pattern = re.compile('((?:(?:January|February|March|April|May|June|July|August|September|October|November|December) (?:[1-3]?[1-9], )?)?[1-9]{4})')
     return pattern.findall(date)
 
+def findPlaces(place):
+    pattern = re.compile('(?:[0-9]+ [A-Z][a-z]+)|(?:[A-Z][a-z]+)|(?:[0-9]+ [A-Z][a-z]+ [A-Z][a-z]+)|(?:[A-Z][a-z]+ [A-Z][a-z]+)|(?:[A-Z][a-z]+, [A-Z][a-z]+)|(?:[A-Z][a-z]+ [A-Z][a-z]+, [A-Z][a-z]+)')
+    return pattern.findall(place)
+
 def findQuery(q):
     if q.find("who") > -1:
         return 1
@@ -43,7 +47,7 @@ def findQuery(q):
         return 2
     elif q.find("where") > -1:
         return 3
-                                   
+                                             
 print getQuery("Who plays Spiderman?")
 print getQuery("Where is the Empire State Building")
 print getQuery("When were the Americas discovered?")
